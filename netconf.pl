61db22a5cae59b4be44cbb47b510220d6283b449
sumikawa <sumikawa@c27e49f0-d0dd-db11-86f2-004063dcf3ec>
2007-03-27 12:29:45 +0000

    hoge

--- netconf.pl
+++ netconf.pl
@@ -1,12 +1,11 @@
 #!/usr/local/bin/perl
-#$Id$
 
 use strict;
 use HTTP::Cookies;
 use SOAP::Lite;
 #use SOAP::Lite +trace => 'debug';
 
-my $target = shift || "192.168.2.3";
+my $target = shift || die "usage: $0 [Alaxala product's IP address]\n";
 my $proxy = "http://$target:832/onapi/";  # SOAP service URL of Alaxala box
 my $namespace = "urn:ietf:params:xml:ns:netconf:base:1.0";
 
@@ -16,39 +15,41 @@ $service->serializer
     ->envprefix("soapenv")
     ->encodingStyle(""); # remove encoding attributes from the envelope header
 
-hello();
-my $result = getconfig();
-
-print $result->valueof('//rpc-reply/rpc-reply/data/ConfigData');
+hello($service);
+print getconfig($service)->valueof('//rpc-reply/rpc-reply/data/ConfigData');
 
 sub hello
 {
 # build and send a "hello" message
 # this is like "Omajinai".  You don't need to modify this funxtion
-my @hello = (
-    SOAP::Data->name("capabilities" => \SOAP::Data->value(
-			 SOAP::Data->name('capability' => "net:alaxala:oan:onapi:1.1"),
-			 SOAP::Data->name('capability' => "urn:ietf:params:xml:ns:netconf:capability:startup:1.0"),
-			 SOAP::Data->name('capability' => "urn:ietf:params:xml:ns:capability:writable-running:1.0"),    
-			 SOAP::Data->name('capability' => $namespace),
-		     )),
-    SOAP::Data->name("session-id"),
-    );
+    my $service = shift;
+    my @hello = (
+	SOAP::Data->name("capabilities" => \SOAP::Data->value(
+			     SOAP::Data->name('capability' => "net:alaxala:oan:onapi:1.1"),
+			     SOAP::Data->name('capability' => "urn:ietf:params:xml:ns:netconf:capability:startup:1.0"),
+			     SOAP::Data->name('capability' => "urn:ietf:params:xml:ns:capability:writable-running:1.0"),    
+			     SOAP::Data->name('capability' => $namespace),
+			 )),
+	SOAP::Data->name("session-id"),
+	);
 
-$service->call(SOAP::Data->name('hello')
-	       ->attr({xmlns => $namespace}) 
-	       => @hello);
+    return $service->call(SOAP::Data->name('hello')
+			  ->attr({xmlns => $namespace}) 
+			  => @hello);
 }
 
 sub getconfig
 {
-my $rpc3 = SOAP::Data->name("source" => \SOAP::Data->value(
-				SOAP::Data->name('running')));
-my $rpc2 = SOAP::Data->name("get-config" => \SOAP::Data->value($rpc3));
-my $rpc1 = SOAP::Data->name("rpc" => \SOAP::Data->value($rpc2))
-    ->attr({"message-id" => "710"});
-
-return $service->call(SOAP::Data->name('rpc')
-		      ->attr({'xmlns' => $namespace})
-		      => \SOAP::Data->value($rpc1));
+    my $service = shift;
+    my $rpc3 = SOAP::Data->name("source" => \SOAP::Data->value(
+				    SOAP::Data->name('running')));
+    my $rpc2 = SOAP::Data->name("get-config" => \SOAP::Data->value($rpc3));
+    my $rpc1 = SOAP::Data->name("rpc" => \SOAP::Data->value($rpc2))
+	->attr({"message-id" => "710"});
+    
+    return $service->call(SOAP::Data->name('rpc')
+			  ->attr({'xmlns' => $namespace})
+			  => \SOAP::Data->value($rpc1));
 }
+
+#$Id$
